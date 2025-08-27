564a733be7dfefa07cd519dc6b94b22c274217d5
Danscot <scotfielddaniel@gmail.com>
2025-08-27 23:37:18 +0100

    Small updates

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    // Add other file paths if needed, such as:
+    // './src/**/*.jsx',
+    // './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
